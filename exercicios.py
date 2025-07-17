166c72d45dd395daac8a7fcc60a4f8a31f70558e
Cesar Outl <cesar71.oliveira@outlook.com>
2025-07-17 14:41:35 -0300

    Commit exercicios

---
+++ exercicios.py
@@ -0,0 +1,20 @@
+    # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+soma = num1 + num2
+
+print(f"O valor da soma {num1} + {num2} é: {soma}")  # Lembrar do f para as variáveis e deixar tudo dentro das ""
+
+    # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+numero = int(input("Digite um número: "))
+div = 5
+resto = numero % div
+
+print(f"O resto da divisão do número por 5 de é: {resto}")
+
+    # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+resultado = num1 * num2
+
+print(f"O valor da multiplicação de {num1} * {num2} é: {resultado}") # Lembrar do f para as variáveis e deixar tudo dentro das ""
